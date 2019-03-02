ce2de0f60dd3ae86a7d5e7152c3aa2dc939aadc5
Rahat Zaman <rahatzamancse@gmail.com>
2019-03-02 18:41:34 +0600

    added statics

--- tourx_website/settings.py
+++ tourx_website/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,4 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static", "static-root")
+# STATICFILES_STORAGE = "whitenoise.djangoGzipManifestStaticFilesStorage"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
 django_heroku.settings(locals())
